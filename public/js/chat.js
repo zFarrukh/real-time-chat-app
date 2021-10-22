57226869ae6317b844e1139a4440aba1aadf3ff5
zFarrukh <diplomatfarruh@gmail.com>
2021-10-22 21:30:44 +0500

    broadcast events and refactore code

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,13 @@
-io()
+const socket = io();
+
+const form = document.getElementById('form');
+const input = document.getElementById("message");
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+    const message = input.value;
+    socket.emit("sendMessage", message);
+})
+
+socket.on('message', (message) => {
+    console.log(message);
+})
--- src/index.js
+++ src/index.js
@@ -13,9 +13,23 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.json());
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log("New web socket connection");
-})
+
+    socket.emit('message', "Welcome!:)");
+    socket.broadcast.emit('message', 'A new user has joined')
+
+    socket.on("sendMessage", (message) => {
+        io.emit("message", message);
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left');
+    });
+
+});
+
+
 
 
 server.listen(port, () => {
