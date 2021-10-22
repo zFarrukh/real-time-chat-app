dc427e6d080489a29e115b244a56cb1ba522ba87
zFarrukh <diplomatfarruh@gmail.com>
2021-10-22 20:43:47 +0500

    add socket.io to project and some refactoring

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -9,11 +13,11 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.json());
 app.use(express.static(publicDirectoryPath));
 
-// app.get('/', (req, res) => {
-//     res.sendFile('index.html');
-// })
-
-
-app.listen(port, () => {
-    console.log("Server is running on port: " + port);
+io.on('connection', () => {
+    console.log("New web socket connection");
 })
+
+
+server.listen(port, () => {
+    console.log("Server is running on port: " + port);
+});
