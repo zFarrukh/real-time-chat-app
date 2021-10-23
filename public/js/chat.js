8288ef52dc1b1eaae1d316609225323be9ccab69
zFarrukh <diplomatfarruh@gmail.com>
2021-10-23 13:05:17 +0500

    form and button disable enable

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,12 +3,17 @@ const socket = io();
 
 const form = document.getElementById('form');
 const input = document.getElementById("message");
+const button = document.getElementById("button");
 const sendLocationButton = document.getElementById("send-location");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault(); 
+    button.setAttribute('disabled', 'disabled');
     const message = input.value;
     socket.emit("sendMessage", message, (error) => {
+        button.removeAttribute('disabled');
+        input.value = '';
+        input.focus();
         if(error) return console.log(error);
 
         console.log("Message delivered");
@@ -22,10 +27,11 @@ socket.on('message', (message) => {
 
 sendLocationButton.addEventListener('click', () => {
     if(!navigator.geolocation) return alert('Geolocation is not supported by your browser');
-
+    sendLocationButton.setAttribute('disabled', 'disabled');
     navigator.geolocation.getCurrentPosition((position) => {
         const {longitude, latitude } = position.coords;
         socket.emit("sendLocation", {longitude, latitude}, () => {
+            sendLocationButton.removeAttribute('disabled');
             console.log("Location shared!");
         });
     })
